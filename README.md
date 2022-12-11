ff32101fdd0a616f841b0a3edf1aceea5384cfd5
LJRollerII <ljcoolkid@optonline.net>
2022-12-10 23:17:50 -0500

    Updates to code, visualizations, and README.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Tech_Layoffs
+# Tech Layoffs
+
+# Background
+
+When the COVID-19 pandemic hit, the tech industry experienced an economic slow down. Due to slow consumer spending, higher interest rates by central banks, and strong dollars overseas hinting towards possible recession, tech firms have started laying employees off. Companies such as Meta, Amazon, and Twitter have laid off a significant percentage of their employees and this trend does not seem to be stopping anytime soon.
+
+# Project Aim
+
+Explore and analyze the data to see if we find any patterns, trends, or interesting information regarding the tech layoffs.
+
+*<u>Note</u>*: Data collected and analyzed for this project is from March 11, 2020 to November 16, 2022.
+
+# Visualizations
+
+[Tech Layoffs 2020 - 2022](https://public.tableau.com/app/profile/lance.roller/viz/TechLayoffs2020-2022/TechLayoffs2020-2022?publish=yes)
+
+[Tech Layoffs in the United States](https://app.powerbi.com/groups/me/reports/3f6a7086-2bf5-4c1b-9997-5805a3387fb1/ReportSection)
+
+*<u>Note</u>*: There are more visualizations in the images section of this repository.
+
+# Data Sources & Tools
+
+**Tools**
+
+Python, Jupyter Notebook, SQL and Excel for gathering and analysis of data.
+
+Tableau and Power BI for data visualizations.
+
+**Data Source**
+
+[Tech Layoff Data](https://www.kaggle.com/datasets/swaptr/layoffs-2022)
--- scripts/layoffs.sql
+++ scripts/layoffs.sql
@@ -12,18 +12,38 @@ FROM layoffs
 WHERE total_laid_off > 199.8
 ORDER BY total_laid_off DESC
 
+
+
 SELECT COUNT(*)
 FROM layoffs
 WHERE total_laid_off > 199.8
 
 --239 companies had a higher than average number of layoffs during this time period (15%).
 
--- 2. Which companies layoff percentage were higher than average?
+
+
+-- 2. What was the lowest number of layoffs for a company?
+SELECT MIN(total_laid_off) AS least_layoffs
+FROM layoffs;
+-- 3
+
+
+
+-- 3. What was the highest number of layoffs for a company?
+SELECT MAX(total_laid_off) AS least_layoffs
+FROM layoffs;
+-- 11000
+
+
+
+-- 4. Which companies layoff percentage were higher than average?
 -- Average number of layoff percentage for this dataset is 0.2786838374291115 
 SELECT *
 FROM layoffs
 WHERE percentage_laid_off > 0.2786838374291115
 ORDER BY percentage_laid_off DESC
+
+
 
 SELECT COUNT(*)
 FROM layoffs
@@ -31,12 +51,30 @@ WHERE percentage_laid_off > 0.2786838374291115
 
 --342 companies had a higher layoff percentage than average (21.6%).
 
--- 3. Which companies raised higher than average funds?
+
+
+-- 5. What is the lowest layoff percentage for a company?
+SELECT MIN(percentage_laid_off) AS lowest_pct
+FROM layoffs;
+-- 0.0
+
+
+
+-- 6. What is the highest layoff percentage for a company?
+SELECT MAX(percentage_laid_off) AS hihest_pct
+FROM layoffs;
+-- 1.0
+
+
+
+-- 7. Which companies raised higher than average funds?
 -- Average number of funds raised for this dataset is 899.8921644159356
 SELECT *
 FROM layoffs
 WHERE funds_raised > 899.8921644159356
 ORDER BY funds_raised DESC
+
+
 
 SELECT COUNT(*)
 FROM layoffs
@@ -44,22 +82,147 @@ WHERE funds_raised > 899.8921644159356
 
 -- 168 companies raised higher than average funds (10.6%).
 
--- 4. Which country has the most compnaies?
+
+
+-- 8. What was the lowest amount of funds raised?
+SELECT MIN(funds_raised) AS lowest_funds
+FROM layoffs;
+-- 0.0
+
+
+
+-- 9. What was the highest amount of funds raised?
+SELECT MAX(funds_raised) AS highest_funds
+FROM layoffs;
+-- 121900
+
+
+
+-- 10. Which country has the most compnaies?
 SELECT country, COUNT(company) AS num_companies
 FROM layoffs
 GROUP BY country
 ORDER BY num_companies DESC
 -- United States has the most companies
 
---5. How many unique companies are there?
+
+
+--11. How many unique companies are there?
 SELECT COUNT(DISTINCT company) 
 FROM layoffs
 -- 1334 companies are unique (84.1%)
 -- 252 companies are not unique (15.9%)
 
---6. Which industry had the largest number of layoffs?
+
+
+--12. Which industry had the largest number of layoffs?
 SELECT industry, COUNT(industry) AS companies_in_industry
 FROM layoffs
 GROUP BY industry
 ORDER BY companies_in_industry DESC
 -- Finance had the most companies
+
+
+
+-- 13. What location had the most layoffs?
+SELECT location, COUNT(location) AS companies_in_location
+FROM layoffs
+GROUP BY location
+ORDER BY companies_in_location DESC
+-- SF Bay Area had the most layoffs
+
+
+
+-- 14. What stage of the comapany had the most layoffs?
+SELECT stage, COUNT(stage) AS num_of_companies
+FROM layoffs
+GROUP BY stage
+ORDER BY num_of_companies DESC
+-- If we don't include Unkown which had 263 companies, the highest stage is Series B at 230.
+
+
+
+-- 15. Which years had the least and most layoffs?
+SELECT EXTRACT(YEAR FROM date) AS year,
+COUNT(EXTRACT(YEAR FROM date)) AS num_of_layoffs
+FROM layoffs
+GROUP BY year
+ORDER BY num_of_layoffs DESC
+--2022 had the most layoffs, 2021 had the least layoffs.
+
+
+
+-- 16. What are the top 10 companies with the highest number of layoffs in the United States?
+SELECT company, total_laid_off, percentage_laid_off, country
+FROM layoffs
+WHERE country = 'United States'
+AND total_laid_off IS NOT NULL
+ORDER BY total_laid_off DESC
+LIMIT 10
+
+
+
+-- 17. How many layoffs were there each year in the United States?
+SELECT EXTRACT(YEAR FROM date) AS year,
+COUNT(EXTRACT(YEAR FROM date)) AS num_of_layoffs
+FROM layoffs
+WHERE country = 'United States'
+GROUP BY year
+ORDER BY num_of_layoffs DESC
+
+
+
+-- 18. What were the top 5 companies with the most and least amount of layoffs in the United States?
+SELECT company, total_laid_off
+FROM layoffs
+WHERE country = 'United States'
+AND total_laid_off IS NOT NULL
+ORDER BY total_laid_off DESC
+LIMIT 5
+
+
+
+SELECT company, total_laid_off
+FROM layoffs
+WHERE country = 'United States'
+AND total_laid_off IS NOT NULL
+ORDER BY total_laid_off ASC
+LIMIT 5
+
+
+
+-- 19. What were the top 5 companies with the highest and lowest layoff percentages in the United States?
+SELECT company, percentage_laid_off
+FROM layoffs
+WHERE country = 'United States'
+AND percentage_laid_off IS NOT NULL
+ORDER BY percentage_laid_off DESC
+LIMIT 5
+
+
+
+SELECT company, percentage_laid_off
+FROM layoffs
+WHERE country = 'United States'
+AND percentage_laid_off IS NOT NULL
+ORDER BY percentage_laid_off ASC
+LIMIT 5
+
+-- 20. What were the top industries in the United States with the most and least amount of layoffs?
+SELECT industry, COUNT(industry) AS companies_in_industry
+FROM layoffs
+WHERE country = 'United States'
+AND industry IS NOT NULL
+GROUP BY industry
+ORDER BY companies_in_industry DESC
+LIMIT 5
+
+
+
+SELECT industry, COUNT(industry) AS companies_in_industry
+FROM layoffs
+WHERE country = 'United States'
+AND industry IS NOT NULL
+GROUP BY industry
+ORDER BY companies_in_industry ASC
+LIMIT 5
